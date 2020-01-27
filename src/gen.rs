e34a8a5a81a480936dc13ca708aa1b684aed5105
Nicholas Bishop <nicholasbishop@gmail.com>
2020-01-27 01:22:08 -0500

    Remove some unnecessary allows

--- src/gen.rs
+++ src/gen.rs
@@ -1,5 +1,5 @@
-#![allow(dead_code, mutable_transmutes, non_camel_case_types, non_snake_case,
-         non_upper_case_globals, unused_assignments, unused_mut,
+#![allow(dead_code, non_camel_case_types, non_snake_case,
+         unused_assignments, unused_mut,
          clippy::cognitive_complexity)]
 type size_t = libc::c_ulong;
 #[derive(Clone, Debug)]
